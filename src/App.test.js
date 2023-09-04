bb19c478945e558e69d57ff01b252d9d4a96034f
Olavur Ellefsen <oe@tokni.com>
2023-09-04 21:20:40 +0100

    Update test to new content

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders post your message text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/post your message/i);
+  expect(textElement).toBeInTheDocument();
 });
